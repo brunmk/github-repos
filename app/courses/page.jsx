19061f2dc6a72e49eccf4826e03a2ff811c5d822
brunmk <brunmk@gmail.com>
2023-09-06 12:33:32 +0100

    change to server comp

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,30 +1,12 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Courses from "../components/Courses";
 
 const AboutPage = () => {
-  const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchCourses = async () => {
-      const response = await fetch("/api/courses");
-      const data = await response.json();
-      setCourses(data);
-      setLoading(false);
-    };
-    fetchCourses();
-  }, []);
-
-  if (loading) {
-    return <p className="text-center">Loading...</p>;
-  }
-
   return (
     <div className="container">
       <h1 className="my-4 text-center">Courses</h1>
       <div className="row row-cols-1 row-cols-md-3 g-4">
-        <Courses courses={courses} />
+        <Courses />
       </div>
     </div>
   );
