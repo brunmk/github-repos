83c8962c9ce9a02dfa3c411d6aecf41c180b0351
brunmk <brunmk@gmail.com>
2023-09-19 17:33:46 +0100

    fix courses

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Courses from "../components/Courses";
 
-const AboutPage = () => {
+const AboutPage = (courses) => {
   return (
     <div className="container">
       <h1 className="my-4 text-center">Courses</h1>
       <div className="row row-cols-1 row-cols-md-3 g-4">
-        {/* <Courses /> */}
+        <Courses courses={courses} />
       </div>
     </div>
   );
